4cf15556afc621829a788764b77e3a80710c84fb
SanaAlvi <sanaalvi066@gmail.com>
2020-06-19 13:17:07 +0500

    Update child.js

--- src/child.js
+++ src/child.js
@@ -1,50 +1,87 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { TransactionContext } from './transcontext';
 import './App.css';
 
 function child() {
-    let {transactions} = useContext (TransactionContext)
+    let { transactions, addTransaction } = useContext(TransactionContext)
+    let [newDesc, setDesc] = useState("")
+    let [newAmount, setAmount] = useState(0)
 
-  return (
-    <div className="App">
-        <h2 className="text">Expense Tracker</h2>
-        <h3 className="margin">YOUR BALANCE <br /> $260.00 </h3>
-        <div className="EXPENSE">
-            <h3>INCOME <br /> $500.00</h3>
-            <h3>EXPENSE <br /> $240.00</h3>
+    const handleAddition = (event) => {
+        event.preventDefault();
+        addTransaction({
+            amount: Number(newAmount),
+            description: newDesc
+        })
+        setDesc(' ')
+        setAmount(0)
+    }
+    const getIncome = () => {
+        let income = 0
+        for (var i = 0; i < transactions.length; i++) {
+            if (transactions[i].amount > 0)
+                income += transactions[i].amount
+        }
+        return income
+    }
+    const getExpense = () => {
+        let expense = 0
+        for (var i = 0; i < transactions.length; i++) {
+            if (transactions[i].amount < 0)
+                expense += transactions[i].amount
+        }
+        return expense
+    }
+    return (
+        <div className="App">
+            <h2 className="text">Expense Tracker</h2>
+            <h3 className="margin">YOUR BALANCE <br /> ${getIncome() + getExpense()} </h3>
+            <div className="EXPENSE">
+                <h3>INCOME <br /> $ {getIncome()}</h3>
+                <h3>EXPENSE <br /> $ {getExpense()}</h3>
+            </div>
+            <div>
+                <h3 className="margin">History</h3>
+                <hr />
+
+                <ul className="transaction-list">
+                    {transaction.map((transObj, ind) => {
+                        return (<li key={ind}>
+                            <span>{transObj.description}</span>
+                            <span>$ {transObj.amount}</span>
+                        </li>
+                        )
+                    })}
+                </ul>
+                <h3 className="margin">Add New Transaction</h3>
+                <hr />
+
+                <form className="transaction" onSubmit={handleAddition}>
+                    <label>
+                        <p className="margin">Enter Description <br />
+                            <input type="text"
+                                value={newDesc}
+                                placeholder="Description"
+                                onChange={(ev) => setDesc(ev.target.value)}
+                                required />
+                        </p>
+                    </label>
+                    <label>
+                        <p className="margin">Enter Amount <br />
+                            <input type="number"
+                                value={newAmount}
+                                placeholder="Amount"
+                                onChange={(ev) => setAmount(ev.target.value)}
+                                required />
+                        </p>
+                    </label>
+                    <br />
+
+                    <input type="submit" value="Add Transaction" className="margin" />
+                </form>
+            </div>
         </div>
-        <div>
-            <h3 className="margin">History</h3>
-            <hr />
-
-            <ul className="transaction-list">
-                {transaction.map((transObj,ind)=>{
-                    return (<li key={ind}> 
-                        <span>{transObj.description}</span>
-                        <span>{transObj.amount}</span>
-                    </li>
-        )   
-                })}
-              </ul>
-            <h3 className="margin">Add New Transaction</h3>
-            <hr />
-
-            <form className="transaction">
-                <label>
-                    <p className="margin">Enter Description</p>
-                    <input type="text" className="margin" required/>
-                </label>
-                <label>
-                    <p className="margin">Enter Amount</p>
-                    <input type="number" className="margin" required/>
-                </label>
-                <br />
-
-                <input type="submit" value="Add Transaction" className="margin" />
-            </form>
-        </div>
-    </div>
-  );
+    );
 }
 
 export default child;
